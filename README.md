04540cff29b74206b30af319761ba58d46c1f9d2
Erik Bjäreholt <erik.bjareholt@gmail.com>
2016-04-03 20:45:04 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+x11watcher
+==========
+
+Watches which windows you have open in X11
+
---
+++ x11.py
@@ -0,0 +1,84 @@
+import subprocess
+from subprocess import PIPE
+
+import re
+
+
+def xprop_id(window_id) -> str:
+    cmd = ["xprop"]
+    cmd.append("-id")
+    cmd.append(window_id)
+    p = subprocess.run(cmd, stdout=PIPE)
+    return str(p.stdout, "utf8")
+
+
+def xprop_root() -> str:
+    cmd = ["xprop"]
+    cmd.append("-root")
+    p = subprocess.run(cmd, stdout=PIPE)
+    return str(p.stdout, "utf8")
+
+
+def get_active_window_id():
+    lines = xprop_root().split("\n")
+    client_list = next(filter(lambda x: "_NET_ACTIVE_WINDOW(" in x, lines))
+    wid = re.findall("0x[0-9a-f]*", client_list)[0]
+    return wid
+
+def get_window_ids():
+    lines = xprop_root().split("\n")
+    client_list = next(filter(lambda x: "_NET_CLIENT_LIST(" in x, lines))
+    window_ids = re.findall("0x[0-9a-f]*", client_list)
+    return window_ids
+
+
+def _extract_xprop_field(line):
+    return "".join(line.split("=")[1:]).strip(" \n")
+
+
+def get_xprop_field(fieldname, xprop_output):
+    return list(map(_extract_xprop_field, re.findall(fieldname + ".*\n", xprop_output)))
+
+
+def get_xprop_field_str(fieldname, xprop_output) -> str:
+    return get_xprop_field(fieldname, xprop_output)[0][1:-1]
+
+
+def get_xprop_field_int(fieldname, xprop_output) -> int:
+    return int(get_xprop_field(fieldname, xprop_output)[0])
+
+
+def get_window(wid, active_window=False):
+    s = xprop_id(wid)
+    window = {
+        "id": wid,
+        "active": active_window,
+        "name": get_xprop_field_str("WM_NAME", s),
+        "class": get_xprop_field("WM_CLASS", s),
+        "desktop": get_xprop_field_int("WM_DESKTOP", s),
+        "command": get_xprop_field("WM_COMMAND", s),
+        "role": get_xprop_field("WM_WINDOW_ROLE", s),
+        "pid": get_xprop_field_int("WM_PID", s),
+    }
+
+    return window
+
+
+def get_windows(wids, active_window_id=None):
+    return [get_window(wid, active_window=(wid == active_window_id)) for wid in wids]
+
+
+if __name__ == "__main__":
+    wids = get_window_ids()
+    active_window_id = get_active_window_id()
+    windows = get_windows(wids, active_window_id)
+
+
+    import sys
+    if len(sys.argv) > 1 and sys.argv[1] == "dump":
+        import json
+        print(json.dumps(windows))
+    else:
+        for window in windows:
+            print(window)
+
