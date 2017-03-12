7ce216c37a0386cfa18a232ddb30bd9362bcefad
Johan Bjäreholt <johan@bjareho.lt>
2017-03-12 15:09:03 +0100

    Fixed typechecking

--- aw_watcher_window/config.py
+++ aw_watcher_window/config.py
@@ -5,8 +5,8 @@ from aw_core.config import load_config
 
 default_client_config = ConfigParser()
 default_client_config["aw-watcher-window"] = {
-    "poll_time": 1.0,
-    "update_time": 15.0,
+    "poll_time": "1.0",
+    "update_time": "15.0",
 }
 
 watcher_config = load_config("aw-watcher_window", default_client_config)
