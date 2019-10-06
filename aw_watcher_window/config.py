75cd8dce4e669acfab7a63db89eb1b9e6c6897fb
cunidev <raffaele.tranquillini@gmail.com>
2019-10-06 19:19:22 +0200

    Update config.py

--- aw_watcher_window/config.py
+++ aw_watcher_window/config.py
@@ -6,10 +6,12 @@ from aw_core.config import load_config as _load_config
 def load_config():
     default_client_config = ConfigParser()
     default_client_config["aw-watcher-window"] = {
-        "poll_time": "1.0",
+        "exclude_title": True,
+        "poll_time": "1.0"
     }
     default_client_config["aw-watcher-window-testing"] = {
-        "poll_time": "1.0",
+        "exclude_title": True,
+        "poll_time": "1.0"
     }
 
     # TODO: Handle so aw-watcher-window testing gets loaded instead of testing is on
