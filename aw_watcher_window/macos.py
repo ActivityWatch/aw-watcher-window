01a5f18aede761751a6afa59ea74ccafbed2886b
Kerkko Pelttari <kerk.pelt@gmail.com>
2020-03-24 16:49:51 +0200

    try using a notification observer on macos. still not working

--- aw_watcher_window/macos.py
+++ aw_watcher_window/macos.py
@@ -1,13 +1,32 @@
 from typing import Dict, Optional
-from AppKit import NSWorkspace, NSRunningApplication
+from AppKit import NSObject, NSWorkspace, NSRunningApplication, NSWorkspaceDidActivateApplicationNotification
 from Quartz import (
     CGWindowListCopyWindowInfo,
     kCGWindowListOptionOnScreenOnly,
     kCGNullWindowID
 )
+from PyObjCTools import AppHelper
 
-def get_current_app() -> Optional[NSRunningApplication]:
-    return NSWorkspace.sharedWorkspace().frontmostApplication()
+
+class Observer(NSObject):
+    app = NSWorkspace.sharedWorkspace().frontmostApplication()
+
+    def get_front_app(self) -> NSRunningApplication:
+        return self.app
+    def set_front_app_(self) -> NSRunningApplication:
+        self.app = NSWorkspace.sharedWorkspace().frontmostApplication()
+
+observer = Observer.new()
+NSWorkspace.sharedWorkspace().notificationCenter().addObserver_selector_name_object_(
+        observer,
+        "set_front_app:",
+        NSWorkspaceDidActivateApplicationNotification,
+        None)
+
+AppHelper.runConsoleEventLoop()
+
+def get_current_app() -> NSRunningApplication:
+    return observer.get_front_app()
 
 
 def get_app_name(app: NSRunningApplication) -> str:
@@ -22,6 +41,8 @@ def get_app_title(app: NSRunningApplication) -> str:
     for window in windowList:
         lookupPid = window['kCGWindowOwnerPID']
         if (lookupPid == pid):
+            print(window)
             return window.get('kCGWindowName', 'Non-detected window title')
-    return ""
+
+    return "Couldn't find title by pid"
 
