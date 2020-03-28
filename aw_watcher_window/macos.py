82e90b28cf7f47e030286355cb553fcaac03edb9
Kerkko Pelttari <kerk.pelt@gmail.com>
2020-03-28 17:45:53 +0200

    Use AppKit to figure if we have accessbility perms and pyobjc to ask for them

--- aw_watcher_window/macos.py
+++ aw_watcher_window/macos.py
@@ -1,9 +1,20 @@
+from ApplicationServices import AXIsProcessTrusted
+from AppKit import NSAlert
 import subprocess
 from subprocess import PIPE
 import os
 
 
 def getInfo() -> str:
+    accessibility_permissions = AXIsProcessTrusted()
+    #accessibility_permissions = False
+    if not accessibility_permissions:
+        title = "Missing accessibility permissions"
+        message = "To let ActivityWatch capture window titles grant it accessibility permissions"
+        ok = False
+        cancel = False
+        alert = NSAlert.alertWithMessageText_defaultButton_alternateButton_otherButton_informativeTextWithFormat_(title, ok, 'Cancel' if cancel else None, None, message)
+        alert.runModal()
     cmd = ["osascript", os.path.join(os.path.dirname(os.path.realpath(__file__)), "printAppTitle.scpt")]
     p = subprocess.run(cmd, stdout=PIPE)
     return str(p.stdout, "utf8").strip()
