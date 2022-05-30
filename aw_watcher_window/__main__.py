3a1ecac5c4932e36bfbb7e5e7183a08d283c27e6
Michael Bianco <iloveitaly@gmail.com>
2022-05-30 09:52:57 -0600

    Fix for multiprocess error on macos

--- aw_watcher_window/__main__.py
+++ aw_watcher_window/__main__.py
@@ -1,3 +1,6 @@
 from aw_watcher_window import main
 
-main()
+# without this guard, running __main__.py directly will result in a multiprocess
+# related error on macos. This makes it challenging to debug the window watcher on macos
+if __name__ == "__main__":
+  main()
