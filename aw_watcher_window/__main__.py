61d86216ef2be1c4ce62407de070f745797b3798
Michael Bianco <iloveitaly@gmail.com>
2022-06-02 10:06:32 -0600

    Update aw_watcher_window/__main__.py
    Co-authored-by: Erik Bjäreholt <erik.bjareholt@gmail.com>

--- aw_watcher_window/__main__.py
+++ aw_watcher_window/__main__.py
@@ -3,4 +3,4 @@ from aw_watcher_window import main
 # without this guard, running __main__.py directly will result in a multiprocess
 # related error on macos. This makes it challenging to debug the window watcher on macos
 if __name__ == "__main__":
-  main()
+    main()
