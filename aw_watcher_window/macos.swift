f08afe80cc3af1c62f948abb015be0ad3adcec67
Michael Bianco <iloveitaly@gmail.com>
2023-01-11 06:41:11 -0700

    fix: misc fixes to swift script on macos, incl URL inspection (#69)
    * Missing clean statement in makefile
    
    * Extract url from safari and chrome-like browsers
    
    * Removing system idle time
    
    * Fixed old heartbeat and added log levels
    
    Reason the existing logic wasn't working is the previous heartbeat is not
    sent with the current timestamp to the server does not merge it
    
    * Additional comments
    
    * Update aw_watcher_window/macos.swift
    
    * dont' allocate formatter each time
    
    * Drop timestamp interval
    
    * Drop to millisecond subtraction
    
    * Check for empty tab titles before using them in place of the notification-provided window title

--- aw_watcher_window/macos.swift
+++ aw_watcher_window/macos.swift
@@ -19,6 +19,48 @@ extension SBObject: ChromeWindow, ChromeTab {}
 
 extension SBApplication: ChromeProtocol {}
 
+// https://github.com/tingraldi/SwiftScripting/blob/4346eba0f47e806943601f5fb2fe978e2066b310/Frameworks/SafariScripting/SafariScripting/Safari.swift#L37
+
+@objc public protocol SafariDocument {
+    @objc optional var name: String { get } // Its name.
+    @objc optional var modified: Bool { get } // Has it been modified since the last save?
+    @objc optional var file: URL { get } // Its location on disk, if it has one.
+    @objc optional var source: String { get } // The HTML source of the web page currently loaded in the document.
+    @objc optional var URL: String { get } // The current URL of the document.
+    @objc optional var text: String { get } // The text of the web page currently loaded in the document. Modifications to text aren't reflected on the web page.
+    @objc optional func setURL(_ URL: String!) // The current URL of the document.
+}
+
+@objc public protocol SafariTab {
+    @objc optional var source: String { get } // The HTML source of the web page currently loaded in the tab.
+    @objc optional var URL: String { get } // The current URL of the tab.
+    @objc optional var index: NSNumber { get } // The index of the tab, ordered left to right.
+    @objc optional var text: String { get } // The text of the web page currently loaded in the tab. Modifications to text aren't reflected on the web page.
+    @objc optional var visible: Bool { get } // Whether the tab is currently visible.
+    @objc optional var name: String { get } // The name of the tab.
+    @objc optional func setURL(_ URL: String!) // The current URL of the tab.
+}
+
+@objc public protocol SafariWindow {
+    @objc optional var name: String { get } // The title of the window.
+    @objc optional func id() -> Int // The unique identifier of the window.
+    @objc optional var index: Int { get } // The index of the window, ordered front to back.
+    @objc optional var document: SafariDocument { get } // The document whose contents are displayed in the window.
+    @objc optional func tabs() -> SBElementArray
+    @objc optional var currentTab: SafariTab { get } // The current tab.
+}
+extension SBObject: SafariWindow {}
+
+@objc public protocol SafariApplication {
+    @objc optional func documents() -> SBElementArray
+    @objc optional func windows() -> [SafariWindow]
+    @objc optional var name: String { get } // The name of the application.
+    @objc optional var frontmost: Bool { get } // Is this the active application?
+}
+extension SBApplication: SafariApplication {}
+
+// AW-specific structs
+
 struct NetworkMessage: Codable, Equatable {
   var app: String
   var title: String
@@ -30,13 +72,49 @@ struct Heartbeat: Codable {
   var data: NetworkMessage
 }
 
+enum HeartbeatError: Error {
+  case error(msg: String)
+}
+
 struct Bucket: Codable {
   var client: String
   var type: String
   var hostname: String
 }
 
-// Placeholder values, set in start()
+// there's no builtin logging library on macos which has levels & hits stdout, so we build our own simple one
+// there a complex open source one, but it makes it harder to compile this simple one-file swift application
+let dateFormatter =  DateFormatter();
+
+func logTimestamp() -> String {
+  let now = Date()
+  dateFormatter.timeZone = TimeZone.current
+  dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+  return dateFormatter.string(from: now)
+}
+
+// generate log prefix based on level
+func logPrefix(_ level: String) -> String {
+  return "\(logTimestamp()) [aw-watcher-window-macos] [\(level)]"
+}
+
+let logLevel = ProcessInfo.processInfo.environment["LOG_LEVEL"]?.uppercased() ?? "INFO"
+
+func debug(_ msg: String) {
+  if(logLevel == "DEBUG") {
+    print("\(logPrefix("DEBUG")) \(msg)")
+  }
+}
+
+func log(_ msg: String) {
+  print("\(logPrefix("INFO")) \(msg)")
+}
+
+func error(_ msg: String) {
+  print("\(logPrefix("ERROR")) \(msg)")
+}
+
+// Placeholder values, set in start() from CLI arguments
 var baseurl = "http://localhost:5600"
 // NOTE: this differs from the hostname we get from Python, here we get `.local`, but in Python we get `.localdomain`
 var clientHostname = ProcessInfo.processInfo.hostName
@@ -66,11 +144,13 @@ func start() {
   //  - hostname
   //  - client_id
   let arguments = CommandLine.arguments
+
   // Check that we get 4 arguments
   if arguments.count != 5 {
     print("Usage: aw-watcher-window <url> <bucket> <hostname> <client>")
     exit(1)
   }
+
   baseurl = arguments[1]
   bucketName = arguments[2]
   clientHostname = arguments[3]
@@ -85,45 +165,18 @@ func start() {
 
   createBucket()
 
+  // listen for changes in focused application
   NSWorkspace.shared.notificationCenter.addObserver(
-    main, selector: #selector(main.focusedAppChanged),
+    main,
+    selector: #selector(main.focusedAppChanged),
     name: NSWorkspace.didActivateApplicationNotification,
-    object: nil)
+    object: nil
+  )
+
   main.focusedAppChanged()
-  //detectIdle()
 }
 
-// TODO: This will be unused for now, as aw-watcher-afk handles it
-func detectIdle() {
-  // TODO: read from config
-  let idletimeout = 3 * 60.0;
-  let untilidle = idletimeout - SystemIdleTime()!
-  if untilidle < 0.0 {
-    // Became idle
-
-    // TODO: send proper event
-    sendHeartbeat(Heartbeat(timestamp: Date.now, data: NetworkMessage(app: "", title: "")))
-
-    var monitor: Any?
-    monitor = NSEvent.addGlobalMonitorForEvents(matching: [
-      .mouseMoved, .leftMouseDown, .rightMouseDown, .keyDown,
-    ]) { e in
-      print("User activity detected")
-      NSEvent.removeMonitor(monitor!)
-      if let oldbeat = oldHeartbeat {
-        sendHeartbeat(Heartbeat(timestamp: Date.now, data: oldbeat.data))
-      }
-      detectIdle()
-    }
-
-    return
-  }
-
-  DispatchQueue.main.asyncAfter(deadline: .now() + untilidle) {
-    detectIdle()
-  }
-}
-
+// TODO might be better to have the python wrapper create this before launching the swift application
 func createBucket() {
   let payload = try! encoder.encode(
     Bucket(client: clientName, type: "currentwindow", hostname: clientHostname))
@@ -135,72 +188,90 @@ func createBucket() {
     urlRequest.addValue("application/json", forHTTPHeaderField: "Content-Type")
     let (_, response) = try await URLSession.shared.upload(for: urlRequest, from: payload)
     guard (200...299).contains((response as! HTTPURLResponse).statusCode) else {
-      print("Failed to create bucket")
+      log("Failed to create bucket")
       return
     }
   }
 }
 
 func sendHeartbeat(_ heartbeat: Heartbeat) {
-  // First, send heartbeat ending last event, if event data differs
-  let payload_old: Heartbeat? = oldHeartbeat.flatMap {
-    if $0.data != heartbeat.data {
-      return Heartbeat(timestamp: heartbeat.timestamp, data: $0.data)
-    } else {
-      return Optional<Heartbeat>.none
-    }
+  let oldPayloadDifferent = oldHeartbeat != nil && oldHeartbeat!.data != heartbeat.data
+  let timeSinceLastHeartbeat = oldHeartbeat != nil ? heartbeat.timestamp.timeIntervalSince(oldHeartbeat!.timestamp) : -1.0
+
+  // if you resize a window a ton of events (subsecond) will be fired
+  // we enforce a 1s minimum gap between events to avoid this
+  if timeSinceLastHeartbeat != -1.0 && timeSinceLastHeartbeat <= 0.5 {
+    debug("skipping heartbeat, last heartbeat was sent 1s ago")
+    return
   }
 
-  // Then, send heartbeat starting new event
-  let payload_new = heartbeat
-
-  // TODO: set proper pulsetime
+  // TODO running these async could cause weird state issues since the observer stuff can send a log of heartbeats
+  //      in a short time under certain circumstances, and we don't want to send them all
   Task {
-    if let payload_old = payload_old {
-      let since_last_seconds = heartbeat.timestamp.timeIntervalSince(oldHeartbeat!.timestamp) + 1
+    if oldPayloadDifferent {
+      debug("sending old heartbeat for merging")
+
       do {
-        try await sendHeartbeatSingle(payload_old, pulsetime: since_last_seconds);
+        // unlike the python aw-client library, we do not enforce a `commit_interval` and instead send the old event (which is not invalid)
+        // at the current time with a pulse value equal to the time since this event was originally sent. The aw-server will then merge
+        // this new event with the original event, extending the recorded time spent on this particular window/application.
+
+        let refreshedOldHeartbeat = Heartbeat(
+          // it is important to refresh the hearbeat using the timestamp where the user stopped working on the previous application
+          // more info: https://github.com/ActivityWatch/aw-watcher-window/pull/69
+          // we don't *think* this millisecond subtraction is necessary, but it may be:
+          // https://github.com/ActivityWatch/aw-watcher-window/pull/69#discussion_r987064282
+          timestamp: heartbeat.timestamp - 1.0/1000.0,
+          data: oldHeartbeat!.data
+        )
+
+        try await sendHeartbeatSingle(refreshedOldHeartbeat, pulsetime: timeSinceLastHeartbeat)
       } catch {
-        print("Failed to send heartbeat: \(error)")
+        log("Failed to send old heartbeat: \(error)")
         return
       }
     }
 
     do {
-      let since_last_seconds = heartbeat.timestamp.timeIntervalSince((oldHeartbeat ?? heartbeat).timestamp) + 1
-      try await sendHeartbeatSingle(payload_new, pulsetime: since_last_seconds);
+      let since_last_seconds = heartbeat.timestamp.timeIntervalSince(heartbeat.timestamp) + 1
+      try await sendHeartbeatSingle(heartbeat, pulsetime: since_last_seconds)
     } catch {
-      print("Failed to send heartbeat: \(error)")
+      log("Failed to send heartbeat: \(error)")
       return
     }
 
-    // Assign the latest heartbeat as the old heartbeat
     oldHeartbeat = heartbeat
   }
 }
 
-enum HeartbeatError: Error {
-    case error(msg: String)
-}
-
 func sendHeartbeatSingle(_ heartbeat: Heartbeat, pulsetime: Double) async throws {
   let url = URL(string: "\(baseurl)/api/0/buckets/\(bucketName)/heartbeat?pulsetime=\(pulsetime)")!
+
   var urlRequest = URLRequest(url: url)
   urlRequest.httpMethod = "POST"
   urlRequest.addValue("application/json", forHTTPHeaderField: "Content-Type")
+
   let payload = try! encoder.encode(heartbeat)
   let (_, response) = try await URLSession.shared.upload(for: urlRequest, from: payload)
+
   guard (200...299).contains((response as! HTTPURLResponse).statusCode) else {
     throw HeartbeatError.error(msg: "Failed to send heartbeat: \(response)")
   }
-  // TODO: remove this debug logging when done
-  print("[heartbeat] timestamp: \(heartbeat.timestamp), pulsetime: \(round(pulsetime * 10) / 10), app: \(heartbeat.data.app), title: \(heartbeat.data.title)")
+
+  debug("[heartbeat] bucket: \(bucketName), timestamp: \(heartbeat.timestamp), pulsetime: \(round(pulsetime * 10) / 10), app: \(heartbeat.data.app), title: \(heartbeat.data.title), url: \(heartbeat.data.url ?? "")")
 }
 
 class MainThing {
   var observer: AXObserver?
   var oldWindow: AXUIElement?
-  var idle = false
+
+  // list of chrome equivalent browsers
+  let CHROME_BROWSERS = [
+    "Google Chrome",
+    "Google Chrome Canary",
+    "Chromium",
+    "Brave Browser",
+  ]
 
   func windowTitleChanged(
     _ axObserver: AXObserver,
@@ -208,13 +279,20 @@ class MainThing {
     notification: CFString
   ) {
     let frontmost = NSWorkspace.shared.frontmostApplication!
+    let bundleIdentifier = frontmost.bundleIdentifier!
+
+    // calculate now before executing any scripting since that can take some time
+    let nowTime = Date.now
+
     var windowTitle: AnyObject?
     AXUIElementCopyAttributeValue(axElement, kAXTitleAttribute as CFString, &windowTitle)
 
     var data = NetworkMessage(app: frontmost.localizedName!, title: windowTitle as? String ?? "")
 
-    if frontmost.localizedName == "Google Chrome" {
-      let chromeObject: ChromeProtocol = SBApplication.init(bundleIdentifier: "com.google.Chrome")!
+    if CHROME_BROWSERS.contains(frontmost.localizedName!) {
+      debug("Chrome browser detected, extracting URL and title")
+
+      let chromeObject: ChromeProtocol = SBApplication.init(bundleIdentifier: bundleIdentifier)!
 
       let frontWindow = chromeObject.windows!()[0]
       let activeTab = frontWindow.activeTab!
@@ -223,15 +301,40 @@ class MainThing {
         data = NetworkMessage(app: "", title: "")
       } else {
         data.url = activeTab.URL
-        if let title = activeTab.title { data.title = title }
+
+        // the tab title is more accurate and often different than the window title
+        // however, in some cases the binary does not have the right permissions to read
+        // the title properly and will return a blank string
+
+        if let tabTitle = activeTab.title {
+          if(tabTitle != "") { data.title = tabTitle }
+        }
       }
+    } else if frontmost.localizedName == "Safari" {
+      debug("Safari browser detected, extracting URL and title")
+
+      let safariObject: SafariApplication = SBApplication.init(bundleIdentifier: bundleIdentifier)!
+
+      let frontWindow = safariObject.windows!()[0]
+      let activeTab = frontWindow.currentTab!
+
+      // Safari doesn't allow incognito mode to be inspected, so we do not know if we should hide the url
+      data.url = activeTab.URL
+
+      // comment above applies here as well
+      if let tabTitle = activeTab.name {
+        if tabTitle != "" { data.title = tabTitle }
+      }
+
     }
 
-    let heartbeat = Heartbeat(timestamp: Date.now, data: data)
+    let heartbeat = Heartbeat(timestamp: nowTime, data: data)
     sendHeartbeat(heartbeat)
   }
 
   @objc func focusedWindowChanged(_ observer: AXObserver, window: AXUIElement) {
+    debug("Focused window changed")
+
     if oldWindow != nil {
       AXObserverRemoveNotification(
         observer, oldWindow!, kAXFocusedWindowChangedNotification as CFString)
@@ -247,11 +350,14 @@ class MainThing {
   }
 
   @objc func focusedAppChanged() {
+    debug("Focused app changed")
+
     if observer != nil {
       CFRunLoopRemoveSource(
         RunLoop.current.getCFRunLoop(),
         AXObserverGetRunLoopSource(observer!),
-        CFRunLoopMode.defaultMode)
+        CFRunLoopMode.defaultMode
+      )
     }
 
     let frontmost = NSWorkspace.shared.frontmostApplication!
@@ -268,7 +374,7 @@ class MainThing {
           userData: UnsafeMutableRawPointer?
         ) -> Void in
         guard let userData = userData else {
-          print("Missing userData")
+          log("Missing userData")
           return
         }
         let application = Unmanaged<MainThing>.fromOpaque(userData).takeUnretainedValue()
@@ -276,7 +382,10 @@ class MainThing {
           application.focusedWindowChanged(axObserver, window: axElement)
         } else {
           application.windowTitleChanged(
-            axObserver, axElement: axElement, notification: notification)
+            axObserver,
+            axElement: axElement,
+            notification: notification
+          )
         }
       }, &observer)
 
@@ -287,7 +396,8 @@ class MainThing {
     CFRunLoopAddSource(
       RunLoop.current.getCFRunLoop(),
       AXObserverGetRunLoopSource(observer!),
-      CFRunLoopMode.defaultMode)
+      CFRunLoopMode.defaultMode
+    )
 
     var focusedWindow: AnyObject?
     AXUIElementCopyAttributeValue(focusedApp, kAXFocusedWindowAttribute as CFString, &focusedWindow)
@@ -298,40 +408,10 @@ class MainThing {
   }
 }
 
+// TODO I believe this is handled by the python wrapper so it isn't needed here
 func checkAccess() -> Bool {
   let checkOptPrompt = kAXTrustedCheckOptionPrompt.takeUnretainedValue() as NSString
   let options = [checkOptPrompt: true]
   let accessEnabled = AXIsProcessTrustedWithOptions(options as CFDictionary?)
   return accessEnabled
-}
-
-func SystemIdleTime() -> Double? {
-  var iterator: io_iterator_t = 0
-  defer { IOObjectRelease(iterator) }
-  guard
-    IOServiceGetMatchingServices(kIOMainPortDefault, IOServiceMatching("IOHIDSystem"), &iterator)
-      == KERN_SUCCESS
-  else {
-    return nil
-  }
-
-  let entry: io_registry_entry_t = IOIteratorNext(iterator)
-  defer { IOObjectRelease(entry) }
-  guard entry != 0 else { return nil }
-
-  var unmanagedDict: Unmanaged<CFMutableDictionary>? = nil
-  defer { unmanagedDict?.release() }
-  guard
-    IORegistryEntryCreateCFProperties(entry, &unmanagedDict, kCFAllocatorDefault, 0) == KERN_SUCCESS
-  else { return nil }
-  guard let dict = unmanagedDict?.takeUnretainedValue() else { return nil }
-
-  let key: CFString = "HIDIdleTime" as CFString
-  let value = CFDictionaryGetValue(dict, Unmanaged.passUnretained(key).toOpaque())
-  let number: CFNumber = unsafeBitCast(value, to: CFNumber.self)
-  var nanoseconds: Int64 = 0
-  guard CFNumberGetValue(number, CFNumberType.sInt64Type, &nanoseconds) else { return nil }
-  let interval = Double(nanoseconds) / Double(NSEC_PER_SEC)
-
-  return interval
 }
