af38b35f3af4fb091000d424704d41926ad7e125
Erik Bjäreholt <erik@bjareho.lt>
2023-12-26 14:57:17 +0100

    feat: added hybrid polling method for macOS (#95)
    * feat: added hybrid polling method for macOS
    
    * fix: added 1s to pulsetimes to fix events not merging

--- aw_watcher_window/macos.swift
+++ aw_watcher_window/macos.swift
@@ -177,6 +177,9 @@ func start() {
   )
 
   main.focusedAppChanged()
+
+  // Start the polling timer
+  main.pollingTimer = Timer.scheduledTimer(timeInterval: 10.0, target: main, selector: #selector(main.pollActiveWindow), userInfo: nil, repeats: true)
 }
 
 // TODO might be better to have the python wrapper create this before launching the swift application
@@ -224,11 +227,11 @@ func sendHeartbeat(_ heartbeat: Heartbeat) {
           // more info: https://github.com/ActivityWatch/aw-watcher-window/pull/69
           // we don't *think* this millisecond subtraction is necessary, but it may be:
           // https://github.com/ActivityWatch/aw-watcher-window/pull/69#discussion_r987064282
-          timestamp: heartbeat.timestamp - 1.0/1000.0,
+          timestamp: heartbeat.timestamp - 0.001,
           data: oldHeartbeat!.data
         )
 
-        try await sendHeartbeatSingle(refreshedOldHeartbeat, pulsetime: timeSinceLastHeartbeat)
+        try await sendHeartbeatSingle(refreshedOldHeartbeat, pulsetime: timeSinceLastHeartbeat + 1)
       } catch {
         log("Failed to send old heartbeat: \(error)")
         return
@@ -236,8 +239,8 @@ func sendHeartbeat(_ heartbeat: Heartbeat) {
     }
 
     do {
-      let since_last_seconds = heartbeat.timestamp.timeIntervalSince(heartbeat.timestamp) + 1
-      try await sendHeartbeatSingle(heartbeat, pulsetime: since_last_seconds)
+      let since_last_seconds = oldHeartbeat != nil ? heartbeat.timestamp.timeIntervalSince(oldHeartbeat!.timestamp) : 0
+      try await sendHeartbeatSingle(heartbeat, pulsetime: since_last_seconds + 1)
     } catch {
       log("Failed to send heartbeat: \(error)")
       return
@@ -267,6 +270,7 @@ func sendHeartbeatSingle(_ heartbeat: Heartbeat, pulsetime: Double) async throws
 class MainThing {
   var observer: AXObserver?
   var oldWindow: AXUIElement?
+  var pollingTimer: Timer?
 
   // list of chrome equivalent browsers
   let CHROME_BROWSERS = [
@@ -275,6 +279,29 @@ class MainThing {
     "Chromium",
     "Brave Browser",
   ]
+
+  @objc func pollActiveWindow() {
+    debug("Polling active window")
+
+    guard let frontmost = NSWorkspace.shared.frontmostApplication else {
+      log("Failed to get frontmost application from polling")
+      return
+    }
+
+    let pid = frontmost.processIdentifier
+    let focusedApp = AXUIElementCreateApplication(pid)
+
+    var focusedWindow: AnyObject?
+    AXUIElementCopyAttributeValue(focusedApp, kAXFocusedWindowAttribute as CFString, &focusedWindow)
+
+    if focusedWindow != nil {
+      focusedWindowChanged(observer!, window: focusedWindow as! AXUIElement)
+    }
+  }
+
+  deinit {
+    pollingTimer?.invalidate()
+  }
 
   func windowTitleChanged(
     _ axObserver: AXObserver,
