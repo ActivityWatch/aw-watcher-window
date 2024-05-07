f822183100237f8c3e815e707b05d1b11f279957
Ignacio Perez <67933444+BasileusErwin@users.noreply.github.com>
2024-05-07 06:30:28 -0300

    Update aw_watcher_window/main.py
    Co-authored-by: Erik Bjäreholt <erik.bjareholt@gmail.com>

--- aw_watcher_window/main.py
+++ aw_watcher_window/main.py
@@ -130,11 +130,10 @@ def heartbeat_loop(client, bucket_id, poll_time, strategy, exclude_title=False, 
         if current_window is None:
             logger.debug("Unable to fetch window, trying again on next poll")
         else:
-            if exclude_titles:
-                for title in exclude_titles:
-                    pattern = re.compile(re.escape(title), re.IGNORECASE)
-                    if pattern.search(current_window["title"]):
-                        current_window["title"] = "excluded"
+            for title in exclude_titles:
+                pattern = re.compile(re.escape(title), re.IGNORECASE)
+                if pattern.search(current_window["title"]):
+                    current_window["title"] = "excluded"
 
             if exclude_title:
                 current_window["title"] = "excluded"
